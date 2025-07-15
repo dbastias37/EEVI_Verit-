c8c58127f8ec9d70de49112c48e58ef774b3ba02
dbastias37 <dbastias.alfaro@gmail.com>
2025-07-15 17:25:10 -0400

    Add Firestore integration

---
+++ scripts/firebase_init.py
@@ -0,0 +1,7 @@
+import firebase_admin
+from firebase_admin import credentials, firestore
+
+cred = credentials.Certificate('serviceAccountKey.json')
+app = firebase_admin.initialize_app(cred)
+
+db = firestore.client()
