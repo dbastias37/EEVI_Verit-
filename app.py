86328185e25ec2f13c8afdede92e3373246f62c4
Diego Bastias Alfaro <dbastias.alfaro@gmail.com>
2025-07-21 23:53:46 -0400

    Remove Flask-AppBuilder

--- app.py
+++ app.py
@@ -1,38 +1,28 @@
 from flask import Flask, flash, redirect
 from flask_sqlalchemy import SQLAlchemy
-from flask_appbuilder import AppBuilder, Model
-from flask_appbuilder.models.sqla.interface import SQLAInterface
-from flask_appbuilder.views import ModelView
-from flask_appbuilder.security.manager import AUTH_DB
+
 from sqlalchemy import Column, Integer, String, or_
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
 app.config['SECRET_KEY'] = 'supersecretkey'
 app.config['CSRF_ENABLED'] = True
-app.config['AUTH_TYPE'] = AUTH_DB
-app.config['AUTH_USER_REGISTRATION'] = True
-app.config['AUTH_USER_REGISTRATION_ROLE'] = 'Public'
 
+# Base de datos
 db = SQLAlchemy(app)
-appbuilder = AppBuilder(app, db.session)
-
-# Desactivar la advertencia de TRACK_MODIFICATIONS
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+# Crea las tablas automaticamente
+with app.app_context():
+    db.create_all()
 # DB init skipped for Render deployment
 
 # Modelo
-class Recurso(Model):
+class Recurso(db.Model):
     id = Column(Integer, primary_key=True)
     nombre = Column(String(150), unique=True, nullable=False)
     descripcion = Column(String(500))
 
-# Vista
-class RecursoView(ModelView):
-    datamodel = SQLAInterface(Recurso)
-
-
-appbuilder.add_view(RecursoView, "Recursos", icon="fa-folder", category="Contenido")
 
 if __name__ == "__main__":
     app.run(debug=True)
