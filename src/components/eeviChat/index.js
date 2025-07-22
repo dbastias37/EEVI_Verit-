a1f4337e9001f52a110dd029da44866d31948741
Diego Bastias Alfaro <dbastias.alfaro@gmail.com>
2025-07-22 13:13:41 -0400

    Delete src directory

--- src/components/eeviChat/index.js
+++
@@ -1,2 +0,0 @@
-export { default as ChatModal } from './ChatModal';
-export { default as UserNavPanel } from './UserNavPanel';
--- src/forum-chat-bootstrap.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { createRoot } from 'react-dom/client';
-import ForumChatOverlay from './components/ForumChatOverlay';
-
-const chatRoot = document.getElementById('chat-root');
-if (chatRoot) {
-  createRoot(chatRoot).render(<ForumChatOverlay />);
-}
