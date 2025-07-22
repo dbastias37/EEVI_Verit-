d4d95b183d4b9a06f9ba2d12e10c4f3dfd4cd905
Diego Bastias Alfaro <dbastias.alfaro@gmail.com>
2025-07-22 19:11:58 -0400

    Update vite.config.ts

--- static/chat-widget/vite.config.ts
+++ static/chat-widget/vite.config.ts
@@ -1,14 +1,7 @@
 import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-
 export default defineConfig({
-  plugins: [react()],
-  build: {
-    outDir: 'dist',
-    emptyOutDir: true,
-    lib: {
-      entry: 'src/main.tsx',
-      formats: ['esm']
-    }
+  // …
+  define: {
+    'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV || 'production')
   }
 });
