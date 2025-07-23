d15ce94b3f4a13659ce2c2bdb463891774c8f2d7
Diego Bastias Alfaro <dbastias.alfaro@gmail.com>
2025-07-22 21:39:09 -0400

    feat: enable persistent chat

--- routes/chat_api.py
+++ routes/chat_api.py
@@ -8,13 +8,13 @@ def _mgr():
     return ChatManager(current_app.config['DB_PATH'])
 
 
-@chat_api_bp.get('/api/messages')
+@chat_api_bp.get('/messages')
 def get_messages():
     messages = _mgr().get_messages()
     return jsonify(messages)
 
 
-@chat_api_bp.post('/api/messages')
+@chat_api_bp.post('/messages')
 def post_message():
     data = request.get_json() or {}
     user = data.get('user')
