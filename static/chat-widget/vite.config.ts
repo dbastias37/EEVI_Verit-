17e9d4a5d629cc4158774dec84d7818526371eb9
Diego Bastias Alfaro <dbastias.alfaro@gmail.com>
2025-07-26 20:52:12 -0400

    Update vite.config.ts

--- static/chat-widget/vite.config.ts
+++ static/chat-widget/vite.config.ts
@@ -4,8 +4,8 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: '../dist',
-    emptyOutDir: false,
+    outDir: '../../dist',
+    emptyOutDir: true,
     rollupOptions: {
       input: 'src/index.tsx',
       output: {
