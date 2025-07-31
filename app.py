6a3ae8a896dc651e8e775b76d926a08641ee28d3
Diego Bastias Alfaro <dbastias.alfaro@gmail.com>
2025-07-31 01:53:05 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, request, redirect, url_for, session, jsonify
 from datetime import datetime
 from flask_login import LoginManager, current_user, UserMixin
+from dotenv import load_dotenv
 
 import os
 import logging
